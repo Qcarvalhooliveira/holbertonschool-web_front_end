b04c879e5f74a707bb2879e5978db7dbb9de9ba7
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-01-23 18:42:27 +0100

    Create 9-prime.js

---
+++ Javascript_advanced/9-prime.js
@@ -0,0 +1,26 @@
+    function isPrime(num) {
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    return num > 1;
+}
+
+function countPrimeNumbers() {
+    let count = 0;
+    for (let i = 2; i <= 100; i++) {
+        if (isPrime(i)) {
+            count++;
+        }
+    }
+    return count;
+}
+
+const startTime = performance.now();
+countPrimeNumbers();
+const endTime = performance.now();
+
+const timeUsed = endTime - startTime;
+
+console.log('Execution time of printing countPrimeNumbers was ' + timeUsed + ' milliseconds.');
