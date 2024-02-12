37b0445b634286f2467d74e366d18786d057cff1
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-02-12 14:45:44 +0100

    Creating basic cookie

---
+++ Cookies_local_storage/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development', 
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './0-index.html',
+      filename: '0-index.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './1-index.html',
+      filename: '1-index.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './2-index.html',
+      filename: '2-index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 8080,
+  },
+};
